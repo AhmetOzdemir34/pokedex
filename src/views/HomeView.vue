5e9ce5618d87ebb94ce1beb2860e8f0b7462d495
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-10-04 15:20:35 +0300

    added simple design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="container">
+      <HomeHeader />
+      <HomePokemons/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HomeHeader from "../components/HomeHeader.vue";
+import HomePokemons from "../components/HomePokemons.vue";
 
 @Component({
-  components: {
-    HelloWorld,
+  components : {
+    HomeHeader,
+    HomePokemons
   },
 })
 export default class HomeView extends Vue {}
 </script>
+
+<style scoped>
+   
+</style>
