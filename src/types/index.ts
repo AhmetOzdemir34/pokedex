693f14b4b43c8bacdb45068bdfd350069bbfebc9
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-10-04 15:17:48 +0300

    added 2 interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,28 @@
-export interface FavsPokemon {
+export interface Pokemons {
     id: number,
     name: string,
     height:number,
     weight:number,
     imageUrl: string,
+    base_experience: number,
+    favourite:boolean
+}
+export interface PokemonDetail {
+    id: number,
+    name: string,
+    height:number,
+    weight:number,
+    Img1: string,
+    Img2: string,
+    Img3: string,
+    base_experience: number,
+    stats: {
+        base_stat:number,
+        effort: number,
+        stat: {
+            name: string,
+            url: string
+        }
+    }[],
+    moves: any[]
 }
