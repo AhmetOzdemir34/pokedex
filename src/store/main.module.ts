e79ef0c425019fdbdc9527dbf1339b0f995d5948
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-10-07 17:39:00 +0300

    changed variables in vuex

--- src/store/main.module.ts
+++ src/store/main.module.ts
@@ -11,65 +11,108 @@ import { store } from "./index";
 export default class PokemonModule extends VuexModule {
     pokemons: Pokemons[] = []
     lightMode = true as boolean
-    results: Pokemons[] |null = null //pokemon results
-    results2: Pokemons[] |null = null //moves results
-    
+    results: Pokemons[] = [] //pokemon results
+    moves: Pokemons[] = [] // moves results
+    species: Pokemons[] = []
+    filtered: Pokemons[] = []
+
     get getPokemons() : Pokemons[]{
         return this.pokemons;   
     }
-    get getPermission(){
-        if(this.getResults && this.getResults2){
-            return false;
-        }
-        return true
+    get getFiltered() : Pokemons[]{
+        return this.filtered;   
     }
-    get getResults(){
-        if(!mainStore.results || mainStore.results.length==0){
-            return null;
-        }
+    get getResults(): Pokemons[]{
         return this.results;
     }
-    get getResults2(){
-        if(!mainStore.results2 || mainStore.results2.length==0){
-            return null;
-        }
-        return this.results2;
+    get getMoves(): Pokemons[]{
+        return this.moves;
+    }
+    get getSpecies(): Pokemons[]{
+        return this.species;
     }
     get getMode() : boolean {
         return this.lightMode;
     }
-    get nameASC() : Pokemons[] {
-        return this.pokemons.sort((a,b) => a.name.localeCompare(b.name))
-    }
-    get heightASC() : Pokemons[] {
-        return this.pokemons.sort((a,b) => a.height-b.height)
-    }
-    get weightDESC() : Pokemons[] {
-        return this.pokemons.sort((a,b) => b.weight-a.weight)
-    }
-    get reset() : Pokemons[] {
-        return this.pokemons.sort((a,b) => a.id-b.id)
-    }
-
     @Mutation
-    searching(data:string){
-        this.results = this.pokemons.filter(e => e.name.includes(data));
-        this.results2 = this.pokemons.filter(e => e.moves[0].move.name.includes(data));
+    nameASC(){
+        this.filtered = this.pokemons.sort((a,b) => a.name.localeCompare(b.name))
+    }
+    @Mutation
+    nameDESC(){
+        this.filtered = this.pokemons.sort((a,b) => b.name.localeCompare(a.name))
+    }
+    @Mutation
+    heightASC(){
+        this.filtered = this.pokemons.sort((a,b) => a.height-b.height)
+    }
+    @Mutation
+    heightDESC(){
+        this.filtered = this.pokemons.sort((a,b) => b.height-a.height)
+    }
+    @Mutation
+    weightASC(){
+        this.filtered = this.pokemons.sort((a,b) => a.weight-b.weight)
+    }
+    @Mutation
+    weightDESC(){
+        this.filtered = this.pokemons.sort((a,b) => b.weight-a.weight)
+    }
+    @Mutation
+    reset(){
+        this.filtered = [];
+        this.species = [];
+        this.moves = [];
+        this.results = [];
+        this.pokemons = this.pokemons.sort((a,b) => a.id - b.id);
     }
 
     @Mutation
     addPokemons(data:Pokemons){
         this.pokemons.push(data);
     }
-    
+    @Mutation
+    addResults(data:Pokemons){
+        this.pokemons.push(data);
+    }
     @Mutation
     removePokemons( data:Pokemons){
         this.pokemons.filter((pokemon:Pokemons) => data.id!==pokemon.id);
     }
-
+    @Mutation
+    match(){
+        for (let i = 0; i < this.pokemons.length; i++) {
+            this.results[i] = this.pokemons[i];
+        }
+    }
     @Mutation
     toggleMode(){
         this.lightMode = !this.lightMode; 
+    }
+    @Mutation
+    clearMoves(){
+        this.moves = [];
+    }
+    @Mutation
+    searching(data:string){
+        this.filtered = [];
+        if(!data){
+            this.results = [];
+            this.moves = [];
+            this.species = [];
+        }else{
+            this.results = this.pokemons.filter(e => e.name.includes(data));
+            this.pokemons.forEach(e => {
+                e.moves.forEach(i => {
+                    if(i.move.name.includes(data)){
+                        if(this.moves.every(ex => ex.id != e.id)){
+                            this.moves.push(e);
+                        }
+                    }
+                });
+            });
+            this.species = this.pokemons.filter(e => e.species.name.includes(data));
+        }        
     }
 }
 
