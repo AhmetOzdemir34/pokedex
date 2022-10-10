b00f06c67afe23906ba85bd8b175586a0abe5a33
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-10-10 18:22:10 +0300

    generated 2 components

---
+++ src/components/LoginPart.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <h2 class="text-center">{{$t("loginView.login")}}</h2>
+        <div class="inputs">
+            <input type="text" v-model="email" class="inps" :placeholder="getUsernamePlaceholder" />
+            <input type="password" v-model="password" class="inps" :placeholder="getPasswordPlaceholder" />
+            <button type="button" class="btn" @click="signIn">{{$t("loginView.loginButton")}}</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { getAuth, signInWithEmailAndPassword } from '@firebase/auth';
+import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class  extends Vue {
+        email= "";
+        password= "";
+
+        signIn(){
+            signInWithEmailAndPassword(getAuth(), this.email, this.password)
+            .then(()=>{
+                //this.$router.push({name:"home"}).catch((err)=>console.log(err.message));               
+            }).catch(err =>{
+                alert(err.message);
+                
+            })
+        }
+        get getUsernamePlaceholder() : string | undefined{
+            return String(this.$t("loginView.username"));
+        }
+        get getPasswordPlaceholder(){
+            return String(this.$t("loginView.password"));
+        }
+    }
+</script>
+
+<style scoped>
+.inputs{
+    width: 50%;
+    margin: 0 auto;
+}
+.inps{
+    width: 80%;
+    display: block;
+    margin: 10px auto;
+    border: 0;
+    padding: 0.4rem .8rem;
+    font-size: 18px;
+    text-align: center;
+    border-radius: 3px;
+}
+.btn{
+    display: block;
+    margin: 0 auto;
+    border: 0;
+    padding: .3rem .7rem;
+    border-radius: 3px;
+    background-color: dodgerblue;
+    cursor: pointer;
+    color: white;
+}
+</style>
---
+++ src/components/RegisterPart.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+            <h2 class="text-center">{{$t("loginView.register")}}</h2>
+            <div class="inputs">
+                <input type="text" v-model="email" class="inps" :placeholder="getUsernamePlaceholder" />
+                <input type="password" v-model="password" class="inps" :placeholder="getPasswordPlaceholder" />
+                <button type="button" class="btn" @click="register">{{$t("loginView.registerButton")}}</button>
+            </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { createUserWithEmailAndPassword, getAuth } from '@firebase/auth';
+import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class  extends Vue {
+        email= "";
+        password= "";
+
+        register(){
+            createUserWithEmailAndPassword(getAuth(), this.email, this.password)
+            .then((data)=>{
+                console.log(data);
+            }).catch(err =>{
+                alert(err.message);
+                
+            })
+        }
+        get getUsernamePlaceholder(): string | undefined{
+            return String(this.$t("loginView.username"));
+        }
+        get getPasswordPlaceholder(){
+            return String(this.$t("loginView.password"));
+        }
+    }
+</script>
+
+<style scoped>
+.inputs{
+    width: 50%;
+    margin: 0 auto;
+}
+.inps{
+    width: 80%;
+    display: block;
+    margin: 10px auto;
+    border: 0;
+    padding: 0.4rem .8rem;
+    font-size: 18px;
+    text-align: center;
+    border-radius: 3px;
+}
+.btn{
+    display: block;
+    margin: 0 auto;
+    border: 0;
+    padding: .3rem .7rem;
+    border-radius: 3px;
+    background-color: dodgerblue;
+    cursor: pointer;
+    color: white;
+}
+</style>
